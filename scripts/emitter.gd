86ecd7c760a51a8d7dea448ed0452d84c3d2cb40
minkaizen <martin.g.cao@gmail.com>
2025-07-07 10:27:10 +1000

    feat: use BulletMovement and BulletPattern in emitter

--- scripts/emitter.gd
+++ scripts/emitter.gd
@@ -3,93 +3,63 @@ extends Node2D
 const BULLET_SCENE = preload('res://scenes/bullet.tscn')
 
 signal all_bullets_fired
-signal finished
 
-@export var duration = 1.0
-@export var amount = 10
-@export var arc_start = Vector2.LEFT
-@export var arc_degrees = 180.0
-@export var arc_distance = 0
-@export var arc_anti_clockwise = false
-@export var bullet_angle_offset = 0
-@export var oneshot = false
+@export var initial_direction: Vector2 = Vector2.LEFT
+@export var pause_seconds: float = 0.5
+@export var pattern: BulletPattern
 
 @onready var interval_timer = %IntervalTimer
-@onready var current_direction: Vector2 = arc_start
+@onready var pause_timer = %PauseTimer
+@onready var current_direction: Vector2 = initial_direction
 
 var bullets_shot = 0
 var interval: float
 
 func _ready() -> void:
-	if oneshot:
-		call_deferred('do_oneshot')
-	else:
-		interval = duration / amount
+	if not pattern.oneshot:
+		interval = pattern.duration / pattern.amount
 		interval_timer.wait_time = interval
 		interval_timer.one_shot = false
-		interval_timer.connect('timeout', on_interval)
-		interval_timer.start()
-		print("interval is %f" % interval)
-	
+		interval_timer.connect('timeout', do_stagger)
+	call_deferred('do_pattern')
+	pause_timer.timeout.connect(do_pattern)
 	self.connect('all_bullets_fired', on_all_bullets_fired)
 
-func on_interval() -> void:
-	fire_normal_bullet()
-	if amount >= 1:
-		current_direction = current_direction.rotated(deg_to_rad(arc_degrees / (amount -1)))
+func do_pattern():
+	if pattern.oneshot:
+		call_deferred('do_oneshot')
+	else:
+		interval_timer.start()
+
+func do_oneshot() -> void:
+	for i in range(0, pattern.amount):
+		fire_bullet(BULLET_SCENE, pattern.movement)
+		current_direction = current_direction.rotated(deg_to_rad(pattern.arc_degrees / (pattern.amount - 1)))
+	emit_signal('all_bullets_fired')
+
+func do_stagger() -> void:
+	fire_bullet(BULLET_SCENE, pattern.movement)
+	if pattern.amount >= 1:
+		current_direction = current_direction.rotated(deg_to_rad(pattern.arc_degrees / (pattern.amount -1)))
 	bullets_shot += 1
-	if bullets_shot >= amount:
+	if bullets_shot >= pattern.amount:
 		emit_signal('all_bullets_fired')
 
 func on_all_bullets_fired() -> void:
+	bullets_shot = 0
+	current_direction = initial_direction
 	interval_timer.stop()
-	print("all bullets fired")
-	emit_signal('finished')
+	pause_timer.start()
 
-func do_oneshot() -> void:
-	for i in range(0, amount):
-		fire_normal_bullet()
-		current_direction = current_direction.rotated(deg_to_rad(arc_degrees / (amount - 1)))
-	emit_signal('all_bullets_fired')
-
-func fire_accelerating_bullet(spd: float = 20.0, acc: float = 200.0) -> Node2D:
-	var dir = current_direction.rotated(deg_to_rad(bullet_angle_offset))
-	var pos = self.global_position + current_direction * arc_distance
-	var bullet = fire_bullet(pos, dir, spd, acc)
-	return bullet
-
-func fire_normal_bullet() -> Node2D:
-	var spd = 100.0
-	var acc = 0.0
-	var dir = current_direction.rotated(deg_to_rad(bullet_angle_offset))
-	var pos = self.global_position + current_direction * arc_distance
-	var bullet = fire_bullet(pos, dir, spd, acc)
-	return bullet
-
-func fire_wave_bullet() -> Node2D:
-	var spd = 0.0
-	var acc = 50.0
-	var dir = current_direction.rotated(deg_to_rad(bullet_angle_offset))
-	var pos = self.global_position + current_direction * arc_distance
-	var wave_amp = 100.0
-	var wave_freq = 1.0
-	var bullet = fire_bullet(pos, dir, spd, acc, wave_amp, wave_freq)
-	return bullet
-
-func fire_bullet(
-	pos: Vector2,
-	dir: Vector2,
-	speed: float,
-	acc: float,
-	wave_amp: float = 0.0,
-	wave_freq: float = 0.0) -> Node2D:
-
-	var bullet = BULLET_SCENE.instantiate()
-	bullet.acceleration = acc
-	bullet.speed = speed
-	bullet.global_position = pos
-	bullet.direction = dir
-	bullet.wave_amp = wave_amp
-	bullet.wave_freq = wave_freq
+func fire_bullet(scene: PackedScene, mv: BulletMovement) -> Node2D:
+	var bullet = scene.instantiate()
+	bullet.acceleration = mv.acceleration
+	bullet.speed = mv.speed
+	bullet.global_position = self.global_position + current_direction * pattern.arc_distance
+	bullet.direction = current_direction.rotated(deg_to_rad(pattern.bullet_angle_offset))
+	bullet.wave_amp = pattern.movement.wave_amp
+	bullet.wave_freq = pattern.movement.wave_freq
+	bullet.radial_velocity = pattern.movement.radial_velocity
+	bullet.radial_acc = pattern.movement.radial_acc
 	get_parent().add_child(bullet)
 	return bullet
